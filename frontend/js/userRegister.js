dfc7631361b0a8c82e94dcac0ea6e59b5dbd9beb
lennartcm <lennartc.moeller@gmail.com>
2024-09-15 17:21:05 +0200

    /auth/ bei userRegister.js entfernt

--- frontend/js/userRegister.js
+++ frontend/js/userRegister.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
         const password = document.getElementById('password').value;
 
         // Send data to server
-        fetch('/api/auth/register', {
+        fetch('/api/register', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
