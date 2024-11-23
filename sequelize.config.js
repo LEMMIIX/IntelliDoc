cfa5c8a18dba887180296f0815bcd57952480275
lennartcm <lennartc.moeller@gmail.com>
2024-11-23 13:40:25 +0100

    Änderung der sequelize Verbindung, damit sofort die individuellen login Daten aus der ConnectPostgres genommen werden, anstatt die login Daten mehrmals festlegen zu müssen.

--- sequelize.config.js
+++ sequelize.config.js
@@ -1,10 +1,21 @@
-// author: Miray Kilic
 const { Sequelize } = require("sequelize");
+const { dbConfig } = require("./ConnectPostgres");
 
-const sequelize = new Sequelize("IntelliDoc", "postgres", "postgres", {
-  host: "localhost",
-  dialect: "postgres", 
-  logging: false, 
-});
+const sequelize = new Sequelize(
+    dbConfig.database,
+    dbConfig.user,
+    dbConfig.password, 
+    {
+        host: dbConfig.host,
+        dialect: "postgres",
+        logging: false,
+        pool: {
+            max: 5,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    }
+);
 
 module.exports = sequelize;
